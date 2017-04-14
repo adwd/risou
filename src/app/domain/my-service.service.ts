1887de15b366dd28385f6c3bc72f425939b735c0
adwd <adwd@users.noreply.github.com>
2017-04-14 11:14:41 +0900

    add domain logic

--- src/app/domain/my-service.service.ts
+++ src/app/domain/my-service.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class MyServiceService {
 
   constructor() { }
+
+  fetchText(): Observable<string> {
+    return Observable.of('foo').delay(1000);
+  }
 
 }
