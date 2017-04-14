d0e87c33b5c8da077e0cca94df8745f155eb38eb
adwd <adwd@users.noreply.github.com>
2017-04-14 10:55:14 +0900

    cd src/app/component && ng g component my-component -m -e

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [MyComponentComponent],
+  exports: [MyComponentComponent]
 })
 export class ComponentModule { }
